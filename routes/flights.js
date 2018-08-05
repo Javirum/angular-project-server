eff0438c90956faae5085150322602ce16eefc7a
Javier Romero <romeropaolini@gmail.com>
2018-08-05 15:39:34 +0200

    Connect kiwi API

---
+++ routes/flights.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+var axios = require('axios');
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+
+  const searchTerm = req.query.search;
+  const data = {
+    messages: req.flash('error'),
+    sections: []
+  };
+
+  const eventApi = axios.create({
+    baseURL: 'https://api.skypicker.com/flights??flyFrom,to,dateFrom,dateTo'
+  })
+
+  eventApi.get(searchTerm)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(next)
+});
+module.exports = router;
