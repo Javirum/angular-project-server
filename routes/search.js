9d8d7e7817a23d3e2c05a5922911098a894a3257
Javier Romero <romeropaolini@gmail.com>
2018-08-07 17:01:24 +0200

    fix for deploy

--- routes/search.js
+++ routes/search.js
@@ -4,16 +4,11 @@ var axios = require('axios');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  const searchTerm = req.query.eventsIn;
+  const url = `https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EVENTS_KEY}&location.address=${req.query.eventsIn}`
 
-  const eventApi = axios.create({
-    baseURL: `https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EVENTS_KEY}&location.address=searchTerm`
-  })
-
-  eventApi.get(searchTerm)
-    .then((values) => {
-      console.log(values);
-      res.status(200).json(values)
+  axios.get(url)
+    .then((response) => {
+      res.status(200).json(response.data)
     })
     .catch(next);
 });
